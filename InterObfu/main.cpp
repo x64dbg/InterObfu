3c5582a55d047ca0014248473960ad8ca4431f14
mrexodia <mr.exodia.tpodt@gmail.com>
2016-08-30 13:42:05 +0200

    opcode and mnemonic enums ready (copied from capstone)

--- InterObfu/main.cpp
+++ InterObfu/main.cpp
@@ -13,13 +13,13 @@ struct WildcardImm : Immediate
 int main()
 {
     //representation of "push 0x300"
-    auto push300 = Instruction(Opcode::Push, Operand(Immediate(0x300)));
+    auto push300 = Instruction(Opcode::PUSH, Operand(Immediate(0x300)));
 
     //representation of "push imm"
-    auto pushImm = Instruction(Opcode::Push, Operand(WildcardImm()));
+    auto pushImm = Instruction(Opcode::PUSH, Operand(WildcardImm()));
 
     Instruction push300_;
-    push300_.opcode.mnem = Opcode::Push;
+    push300_.opcode.mnem = Opcode::PUSH;
     push300_.opCount = 1;
     push300_.operands[0].type = Operand::Imm;
     push300_.operands[0].imm.val = 0x300;
@@ -28,19 +28,19 @@ int main()
 
     //representation of "mov eax, ebx"
     Instruction movReg;
-    movReg.opcode.mnem = Opcode::Mov;
+    movReg.opcode.mnem = Opcode::MOV;
     movReg.opCount = 2;
     movReg.operands[0].type = Operand::Reg;
-    movReg.operands[0].reg.reg = Register::Eax;
+    movReg.operands[0].reg.reg = Register::EAX;
     movReg.operands[1].type = Operand::Reg;
-    movReg.operands[1].reg.reg = Register::Ebx;
+    movReg.operands[1].reg.reg = Register::EBX;
 
     //representation of "mov [ebx + 0x401000], 0x3"
     Instruction movMem;
-    movMem.opcode.mnem = Opcode::Mov;
+    movMem.opcode.mnem = Opcode::MOV;
     movMem.opCount = 2;
     movMem.operands[0].type = Operand::Mem;
-    movMem.operands[0].mem.base.reg = Register::Ebx;
+    movMem.operands[0].mem.base.reg = Register::EBX;
     movMem.operands[0].mem.disp.val = 0x401000;
     movMem.operands[1].type = Operand::Imm;
     movMem.operands[1].imm.val = 0x3;
